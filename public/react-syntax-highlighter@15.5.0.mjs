c1adae5d2b924f2bd2448176b2d4e3a413d12805
renqabs <130155002+renqabs@users.noreply.github.com>
2023-09-18 20:39:30 +0800

    本地化SyntaxHighlighter

---
+++ public/react-syntax-highlighter@15.5.0.mjs
@@ -0,0 +1,4 @@
+/* esm.sh - react-syntax-highlighter@15.5.0 */
+import "./react.mjs";
+export * from "./react-syntax-highlighter.bundle.mjs";
+export { default } from "./react-syntax-highlighter.bundle.mjs";
