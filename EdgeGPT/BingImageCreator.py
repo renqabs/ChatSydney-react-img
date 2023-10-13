296833e8eed910f9964e76122fcdf51b6336f2f3
renqabs <130155002+renqabs@users.noreply.github.com>
2023-10-13 20:55:50 +0800

    修复画图功能

--- EdgeGPT/BingImageCreator.py
+++ EdgeGPT/BingImageCreator.py
@@ -10,17 +10,13 @@ from functools import partial
 from typing import Dict
 from typing import List
 from typing import Union
-
+from .ip_rand import get_random_ip
 import httpx
 import pkg_resources
 import regex
 import requests
 
 BING_URL = os.getenv("BING_URL", "https://www.bing.com")
-# Generate random IP between range 13.104.0.0/14
-FORWARDED_IP = (
-    f"13.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
-)
 HEADERS = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "accept-language": "en-US,en;q=0.9",
@@ -28,8 +24,8 @@ HEADERS = {
     "content-type": "application/x-www-form-urlencoded",
     "referrer": "https://www.bing.com/images/create/",
     "origin": "https://www.bing.com",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
-    "x-forwarded-for": FORWARDED_IP,
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.41",
+    "x-forwarded-for": get_random_ip(),
 }
 
 # Error messages
@@ -308,7 +304,7 @@ class ImageGenAsync:
                 timeout=200,
             )
             if response.status_code != 302:
-                print(f"ERROR: {response.text}")
+                #print(f"ERROR: {response.text}")
                 raise Exception("Redirect failed")
         # Get redirect URL
         redirect_url = response.headers["Location"].replace("&nfy=1", "")
@@ -347,7 +343,7 @@ class ImageGenAsync:
         for im in normal_image_links:
             if im in bad_images:
                 print("Bad images")
-                # raise Exception("Bad images")
+                #raise Exception("Bad images")
         # No images
         if not normal_image_links:
             raise Exception("No images")
